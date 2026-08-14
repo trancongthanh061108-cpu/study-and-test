3c4ab54368ae16d3c2d17687539c2f54c675a040
trancongthanh061108-cpu <trancongthanh061108@gmail.com>
2026-08-14 16:17:04 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -687,7 +687,7 @@ function _renderExamList(type) {
   }
   filtered.forEach(e => {
     const freeIds = [
-      "vact_de_1",
+      "vact_de_1","vact_de_2","vact_de_3",
       'hsa_kh_1','hsa_kh_2','hsa_kh_3','hsa_kh_h',
       'hsa_toan_1', 'hsa_toan_2', 'hsa_toan_3',
       'hsa_van_1', 'hsa_van_2', 'hsa_van_3',
@@ -1663,7 +1663,7 @@ function clickStartExam(id) {
   const exam = sampleExams.find(e => e.id === id);
 
   const freeIds = [
-    "vact_de_1",
+    "vact_de_1","vact_de_2","vact_de_3",
     'hsa_kh_1','hsa_kh_2','hsa_kh_3','hsa_kh_h',
     'hsa_toan_1', 'hsa_toan_2', 'hsa_toan_3',
     'hsa_van_1', 'hsa_van_2', 'hsa_van_3',
